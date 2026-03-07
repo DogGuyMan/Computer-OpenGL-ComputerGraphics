624bf59357babee21c27386f50b76928c1416d28
DogGuyMan <escatrgot@gmail.com>
2026-03-07 19:40:30 +0900

    [chore] : chapter2 / chapter3 dummy main 추가
    chapter2, chapter3 빌드 타깃이 비어 있어 빌드 실패하는 문제를 막기 위해
    placeholder main.cpp 추가

---
+++ apps/chapter2/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello Chapter2" << std::endl;
+}
---
+++ apps/chapter3/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello Chapter3" << std::endl;
+}
