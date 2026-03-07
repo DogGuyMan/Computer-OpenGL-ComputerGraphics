ea7715cd4e6af51fbcb9ce7dbd20573dccea4990
DogGuyMan <escatrgot@gmail.com>
2026-03-07 20:50:03 +0900

    [dev] : create window and render loop + primitives

--- apps/chapter1/entry.h
+++ apps/chapter1/entry.h
@@ -3,8 +3,35 @@
 
 namespace SJH::Chapter1
 {
-	void reshape(int w, int h);
-	void display(void);
+	void Reshape(int w, int h);
+	void Display1(void);
+	void Display2(void);
+	void Display3(void);
+	void Display4(void);
+	void Display5(void);
+
+	void TestGLPoints(void);
+
+	void TestGLLines(void);
+	void TestGLLineStrip(void); // 추가될때마다 1개씩 더 직선이 그려짐
+	void TestGLLineLoop(void);
+
+	void TestGLTriangles(void);
+	void TestGLTriangleStrip(void); // 추가될때마다 1개씩 더 삼각형이 그려짐
+	void TestGLTriangleFan(void);
+
+	void TestGLQuads(void);
+	void TestGLQuadStrip(void); // 2개 추가될때마다 1개씩 더 사각형이 그려짐
+
+	void TestGLPolygon(void); // 점 개수에 맞게 닫힌 다각형을 그림
+
+	void TestGLCube(void);
+	void TestGLTetrahedron(void);
+	void TestGLIcosahedron(void);
+	void TestGLSphere(void); // slices stacks : 위도 경도
+	void TestGLTorus(void);
+	void TestGLCone(void);
+	void TestGLTeapot(void);
 } // namespace SJH::Chapter1
 
 #endif // __CHAPTER_1_ENTRY_H__
