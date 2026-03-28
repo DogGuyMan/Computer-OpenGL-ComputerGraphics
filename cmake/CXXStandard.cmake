e3788a9015daa3021bd03b895e4cb97e86153bf1
DogGuyMan <escatrgot@gmail.com>
2026-03-28 16:10:06 +0900

    [chore] : 크로스플랫폼 빌드 설정 정리
    - CXXStandard / Dependency CMake 모듈에서 macOS / Windows 분기 정리
    - apps/test.main.cpp 미사용 파일 제거
    - chapter1 진입부 사소한 정돈

--- cmake/CXXStandard.cmake
+++ cmake/CXXStandard.cmake
@@ -1,20 +1,25 @@
 # ______ 타겟에 프로젝트 표준 C++17 + 경고 옵션 적용 ______
 function(set_project_standards target)
     target_compile_features(${target} PRIVATE cxx_std_17)
-    set_target_properties(${target} PROPERTIES
-        CXX_EXTENSIONS OFF
-    )
+    set_target_properties(${target} PROPERTIES CXX_EXTENSIONS OFF)
 
-    # ______ 공통 경고 ______
-    target_compile_options(${target} PRIVATE
-        $<$<CXX_COMPILER_ID:MSVC>: /W4 /permissive- /utf-8>
-        $<$<NOT:$<CXX_COMPILER_ID:MSVC>>: -Wall -Wextra -Wpedantic>
-    )
+    set(_is_msvc "$<CXX_COMPILER_ID:MSVC>")
+    set(_is_debug_msvc "$<AND:$<CONFIG:Debug>,${_is_msvc}>")
+    set(_is_debug_gcc "$<AND:$<CONFIG:Debug>,$<NOT:${_is_msvc}>>")
+    set(_no_error_pound "-Wno-error=#warnings")
 
-    # ______ Debug 전용: -Werror (unused-variable 제외) ______
     target_compile_options(${target} PRIVATE
-        $<$<AND:$<CONFIG:Debug>,$<CXX_COMPILER_ID:MSVC>>: /WX /wd4101 /Od /Zi>
-        $<$<AND:$<CONFIG:Debug>,$<NOT:$<CXX_COMPILER_ID:MSVC>>>:
-            -Werror -Wno-error=unused-variable -Wno-error=unused-parameter -g -O0>
+        # ______ 공통 경고 ______
+        $<${_is_msvc}:           /W4 /permissive- /utf-8>
+        $<$<NOT:${_is_msvc}>:   -Wall -Wextra -Wpedantic>
+        # ______ Debug: warnings-as-errors + 세부 제어 ______
+        $<${_is_debug_msvc}:     /WX /wd4101 /Od /Zi>
+        $<${_is_debug_gcc}:
+            -Werror -g -O0
+            -Wconversion -Wdouble-promotion -Warray-bounds
+            -Wunused-but-set-variable
+            -Wno-error=unused-variable -Wno-error=unused-parameter
+            -Wno-error=unused-but-set-variable -Wno-unused-function
+            ${_no_error_pound}>
     )
 endfunction()
