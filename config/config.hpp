9fcdd226cc030a8f5ad823d5f56a72908088aa09
chorogchip <chorogchip@naver.com>
2026-06-06 21:29:41 +0900

    Switch project build to Visual Studio

---
+++ config/config.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <cstdint>
+#include <string_view>
+
+static constexpr std::string_view PROJECT_NAME = "OpenGL-ComputerGraphics";
+static constexpr std::string_view PROJECT_RESOURCE_PATH = "resources";
